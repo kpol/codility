1b3ad82a55e8b866d5ff5ba4a42e9ca9fb2e203c
kpol <kirpolis@gmail.com>
2019-07-26 11:55:40 +1200

    BinaryGap

---
+++ src/Codility/BinaryGap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Codility
+{
+    class BinaryGap
+    {
+        public int solution(int N)
+        {
+            var firstOne = (int)Math.Log(N, 2);
+            var length = 0;
+            int maxLength = 0;
+            var n = 1 << firstOne;
+
+            while ((n = n >> 1) != 0)
+            {
+                var res = n & N;
+
+                if (res == n && length != 0) // 1
+                {
+                    maxLength = maxLength < length ? length : maxLength;
+                    length = 0;
+                }
+                else //0
+                {
+                    length++;
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ src/Codility/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Codility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = 529;
+
+            Console.WriteLine(Convert.ToString(N, 2));
+
+            var result = new BinaryGap().solution(N);;
+
+            Console.WriteLine(result);
+        }
+    }
+}
