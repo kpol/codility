a41a06a728d3a5f2cc1ea42774eba6b6a4e16763
kpol <kirpolis@gmail.com>
2019-07-26 14:28:43 +1200

    PermCheck

---
+++ src/Codility/PermCheck.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+namespace Codility
+{
+    class PermCheck
+    {
+        public int solution(int[] A)
+        {
+            var bitArray = new BitArray(A.Length);
+
+            foreach (var i in A)
+            {
+                if (i <= 0 || i > A.Length)
+                {
+                    return 0;
+                }
+
+                if (bitArray[i - 1])
+                {
+                    return 0;
+                }
+
+                bitArray[i - 1] = true;
+            }
+
+            foreach (bool b in bitArray)
+            {
+                if (!b)
+                {
+                    return 0;
+                }
+            }
+
+            return 1;
+        }
+    }
+}
