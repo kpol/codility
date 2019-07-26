c724c88249726bff436cdaef8824a5863298fecf
kpol <kirpolis@gmail.com>
2019-07-26 15:35:15 +1200

    MissingInteger

---
+++ src/Codility/MissingInteger.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace Codility
+{
+    class MissingInteger
+    {
+        public int solution(int[] A)
+        {
+            var res = A.Where(i => i > 0).Distinct().OrderBy(i => i);
+
+            var min = 1;
+
+            foreach (var x in res)
+            {
+                if (x != min)
+                {
+                    return min;
+                }
+
+                min++;
+            }
+
+            return min;
+        }
+    }
+}
