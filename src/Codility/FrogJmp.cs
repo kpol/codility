d0b9a7eb5a5bd1bc378cb3932994458bcf65a113
kpol <kirpolis@gmail.com>
2019-07-26 13:58:10 +1200

    FrogJmp

---
+++ src/Codility/FrogJmp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Codility
+{
+    class FrogJmp
+    {
+        public int solution(int X, int Y, int D)
+        {
+            var distance = Y - X;
+
+            return (int) Math.Ceiling((double)distance / D);
+        }
+    }
+}
