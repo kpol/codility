bd1b182da187a6ca3951a17b936b4145dec3e37f
kpol <kirpolis@gmail.com>
2019-07-26 16:17:41 +1200

    TieRopes

---
+++ src/Codility/TieRopes.cs
@@ -0,0 +1,24 @@
+﻿namespace Codility
+{
+    class TieRopes
+    {
+        public int solution(int K, int[] A)
+        {
+            var length = 0;
+            var count = 0;
+
+            foreach (var rope in A)
+            {
+                length += rope;
+
+                if (length >= K)
+                {
+                    length = 0;
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
