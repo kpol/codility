a81e41389dfc59df5f7f084c76e808ccaeb74c2d
kpol <kirpolis@gmail.com>
2019-07-26 16:30:15 +1200

    PermMissingElem

---
+++ src/Codility/PermMissingElem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Codility
+{
+    class PermMissingElem
+    {
+        public int solution(int[] A)
+        {
+            var set = new HashSet<int>(A);
+
+            for (int i = 0; i < A.Length + 1; i++)
+            {
+                if (!set.Contains(i + 1))
+                {
+                    return i + 1;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
