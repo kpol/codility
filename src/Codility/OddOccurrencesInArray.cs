b6176d17d0726fedc951b152325ff1b56331228e
kpol <kirpolis@gmail.com>
2019-07-26 13:22:48 +1200

    OddOccurrencesInArray

---
+++ src/Codility/OddOccurrencesInArray.cs
@@ -0,0 +1,17 @@
+﻿namespace Codility
+{
+    class OddOccurrencesInArray
+    {
+        public int solution(int[] A)
+        {
+            var x = A[0];
+            
+            for (int i = 1; i < A.Length; i++)
+            {
+                x ^= A[i];
+            }
+
+            return x;
+        }
+    }
+}
