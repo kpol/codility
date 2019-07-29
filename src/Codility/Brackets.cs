f48340a258cf0b4af83b3a089f9240331d7c073f
kpol <kirpolis@gmail.com>
2019-07-29 16:37:35 +1200

    Brackets

---
+++ src/Codility/Brackets.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Codility
+{
+    class Brackets
+    {
+        public int solution(string S)
+        {
+            var stack = new Stack<char>();
+
+            foreach (var c in S)
+            {
+                if (c == '{' || c == '(' || c == '[')
+                {
+                    stack.Push(c);
+                }
+                else if (c == '}' || c == ')' || c == ']')
+                {
+                    if (stack.Count == 0)
+                    {
+                        return 0;
+                    }
+
+                    var open = stack.Pop();
+
+                    if (c == '}' && open != '{'
+                        || c == ')' && open != '('
+                        || c == ']' && open != '[')
+                    {
+                        return 0;
+                    }
+
+                }
+            }
+
+            return stack.Count == 0 ? 1 : 0;
+        }
+    }
+}
--- src/Codility/Program.cs
+++ src/Codility/Program.cs
@@ -7,13 +7,12 @@ namespace Codility
     {
         static void Main(string[] args)
         {
-            var N = 529;
-
-            Console.WriteLine(Convert.ToString(N, 2));
-
-            var result = new CyclicRotation().solution(new int[]{}, 1);
-
-            Console.WriteLine(result);
+            Console.WriteLine((int)'{');
+            Console.WriteLine((int)'}');
+            Console.WriteLine((int)'(');
+            Console.WriteLine((int)')');
+            Console.WriteLine((int)'[');
+            Console.WriteLine((int)']');
         }
     }
 }
